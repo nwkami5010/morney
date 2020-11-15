4289f6967112507fed5c289d95c3b294be7d690e
nwkami <715190184@qq.com>
2020-11-15 18:31:14 +0800

    tags模块

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -2,20 +2,47 @@
   <div class="tags">
 
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)">{{ tag }}</li>
+
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Tags"
+import Vue from 'vue';
+import {Component,Prop} from 'vue-property-decorator';
+
+@Component
+export default class Tags extends Vue{
+ @Prop() readonly  dataSource: string[] | undefined;//只能写全局的构造函数props必须加括号
+ selectedTags: string[]= [];//字符串数组
+
+ toggle(tag: string) {
+   const index =this.selectedTags.indexOf(tag);
+   if(index >=0){
+     this.selectedTags.splice(index, 1);
+   }else {
+     this.selectedTags.push(tag);
+   }
+ }
+
+ create() {
+   const name = window.prompt('请输入标签名');
+   if(name === '') {
+     window.alert('标签名不能为空');
+
+   }else if (this.dataSource) {
+     this.$emit('update:dataSource',
+        [...this.dataSource, name]);
+
+   }
+
+ }
 }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,12 +3,12 @@
   <NumberPad/>
   <Types xxx=""/>
   <Notes/>
-  <Tags/>
+  <Tags ：data-source.sync="tags"/>
 </Layout>
   
 </template>
 
-<script lang="ts">
+<script >
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
@@ -16,7 +16,13 @@ import Tags from '@/components/Money/Tags.vue';
 export default {
   name: "Money",
   components: {Tags, Notes, Types, NumberPad},
-};
+  data(){
+    return{
+      tags:['衣','食','住','行']
+    }
+  },
+
+}
 </script>
 <style lang="scss">
   .layout-content{
